2be0f3cf15c1065d16b953cb325485233d7342b8
kaderisasiceohmsd <kaderisasihmsd2025@gmail.com>
2025-08-13 11:01:50 +0700

    template

---
+++ template_code/main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+# session state agar ketika pindah page tidak berubah data yang tersedia
+
+st.session_state.pindah = True
+
+Homepage = st.Page("Halaman Utama/halaman_utama.py",
+    title="Nama_Kelompok",
+    default=True)
+
+Mahasiswa1 = st.Page(
+    "Buku Kating/117_Nobel Nizam Fathirizki.py",
+    title="117 - Nobel Nizam Fathirizki",
+    icon=":material/person:",
+)
+
+#Perlu diperhatikan perubahannya
+KREASI = st.Page("tools/KREASI.py", title="KREASI", icon=":material/search:")
+KREASII = st.Page("tools/KREASII.py", title="KREASII", icon=":material/search:")
+
+#Perlu diperhatikan perubahannya
+if st.session_state.pindah:
+    pg = st.navigation(
+        {
+            "Halaman Utama": [Homepage],
+            "Buku Kating": [Mahasiswa1],
+            "Try Me !!": [KREASI, KREASII],
+        }
+    )
+else:
+    st.write("Maaf Anda kurang beruntung :(") 
+pg.run()
+
